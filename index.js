8a3bd45bf2cbc9b6b3d0c8f11b96672449ef670b
Vitor Dalbem Costa <dalbemcosta@gmail.com>
2022-02-09 09:14:29 +0000

    lab partialy done

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); 
+    this.length = this.items.length; 
+    return this.items.sort(function(a, b) {
+      return a - b 
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length > pos ) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+    }
 
-  max() {}
+  max() {
+    
+  }
 
-  min() {}
+  min() {
+
+  }
 
   sum() {}
 
